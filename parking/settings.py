cf2f7605d386d3353587175f9f621a3bc1ffac5a
KaysonN <carloskayson@hotmail.com>
2023-04-18 21:33:39 -0300

    Teste

--- parking/settings.py
+++ parking/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 STATIC_URL = "/static/"
 
